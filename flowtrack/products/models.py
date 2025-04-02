40f9a4e2216dd7a7a1230db1d5e39b45389d28d7
Daniel <dkowalski.dev@gmail.com>
2025-04-02 20:57:13 +0200

    Stworzenie modeli produkt i kategoria

--- flowtrack/products/models.py
+++ flowtrack/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True, null=True)
+
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    serial_number = models.CharField(max_length=100, blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    purchase_price = models.FloatField(default=0)
+    sale_price = models.FloatField(default=0)
