008e229f09887ceb5d894f7192b82042336b9520
Daniel <dkowalski.dev@gmail.com>
2025-04-06 21:38:26 +0200

    Dodano choices do region i teraz mamy listę województw do wyboru

--- flowtrack/clients/models.py
+++ flowtrack/clients/models.py
@@ -3,12 +3,31 @@ import uuid
 from django.contrib.auth.models import User
 
 class Client(models.Model):
+    choices = (
+    ('brak', 'Wybierz region z listy'),
+    ('dol', 'Dolnośląskie'),
+    ('kuj', 'Kujawsko-pomorskie'),
+    ('lube', 'Lubelskie'),
+    ('lubu', 'Lubuskie'),
+    ('lod', 'Łódzkie'),
+    ('mal', 'Małopolskie'),
+    ('maz', 'Mazowieckie'),
+    ('opo', 'Opolskie'),
+    ('podk', 'Podkarpackie'),
+    ('podl', 'Podlaskie'),
+    ('pom', 'Pomorskie'),
+    ('slas', 'Śląskie'),
+    ('swie', 'Świętokrzyskie'),
+    ('war', 'Warmińsko-mazurskie'),
+    ('wie', 'Wielkopolskie'),
+    ('zac', 'Zachodniopomorskie'),
+    )
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField(blank=True, default="")
     phone = models.CharField(max_length=16, blank=True, default="")
     address = models.CharField(max_length=120, blank=True, default="")
-    region = models.CharField(max_length=30, blank=True, null=True)
+    region = models.CharField(max_length=30, choices=choices, default='Brak')
 
     class Meta:
         abstract = True
